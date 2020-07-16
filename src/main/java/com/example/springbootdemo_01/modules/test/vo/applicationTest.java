0704396d5e94597d59c078db9947910f6bada2ed
jfyang-2020 <better592600@163.com>
2020-07-16 11:51:18 +0800

    demo1

---
+++ src/main/java/com/example/springbootdemo_01/modules/test/vo/applicationTest.java
@@ -0,0 +1,48 @@
+package com.example.springbootdemo_01.modules.test.vo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:config/applicationTest.properties")
+@ConfigurationProperties(prefix = "com.test")
+public class applicationTest {
+
+    private String name1;
+    private int age1;
+    private String desc1;
+    private String random1;
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+    public int getAge1() {
+        return age1;
+    }
+
+    public void setAge1(int age1) {
+        this.age1 = age1;
+    }
+
+    public String getDesc1() {
+        return desc1;
+    }
+
+    public void setDesc1(String desc1) {
+        this.desc1 = desc1;
+    }
+
+    public String getRandom1() {
+        return random1;
+    }
+
+    public void setRandom1(String random1) {
+        this.random1 = random1;
+    }
+}
---
+++ src/test/java/com/example/springbootdemo_01/Springbootdemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
