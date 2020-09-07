21e6231ee85c0249e9e833d3f6e2f0139bf258dd
jfyang-2020 <better592600@163.com>
2020-09-07 11:45:43 +0800

    "l"

--- src/main/java/com/example/springboot/modules/test/service/impl/CountryServiceImpl.java
+++ src/main/java/com/example/springboot/modules/test/service/impl/CountryServiceImpl.java
@@ -2,11 +2,12 @@ package com.example.springboot.modules.test.service.impl;
 
 import com.example.springboot.modules.test.dao.CountryDao;
 import com.example.springboot.modules.test.entity.Country;
+import com.example.springboot.modules.test.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CountryServiceImpl {
+public class CountryServiceImpl implements CountryService {
 
     @Autowired
     private CountryDao countryDao;
